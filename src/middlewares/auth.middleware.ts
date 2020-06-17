e9b2d9b8b91a6f2eabe60501eff57784e0b4c602
Nikola Krunic <dzony.kurnic@gmail.com>
2020-06-17 18:18:55 +0200

    JWT

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -9,46 +9,55 @@ import { jwtSecret } from "config/jwt.secret";
 export class AuthMiddleware implements NestMiddleware {
     constructor(private readonly administratorService: AdministratorService) {  }
 
-    async use(req: Request, res: Response, next: NextFunction ){
-            if(!req.headers.authorization){
-                throw new HttpException('Token not found', HttpStatus.UNAUTHORIZED);
-            }
-
-            const token = req.headers.authorization;
-
-            const tokenParts = token.split(' ');
-            if(tokenParts.length !== 2){
-                throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
-            }
-            const tokenString = tokenParts[1];
-
-
-            const jwtData: JwtDataAdmnistratorDto = jwt.verify(token, jwtSecret);
-            if(!jwtData){
-                throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
-            } 
-
-            const ip = req.ip.toString();
-
-            if(jwtData.ip !== req.ip.toString()){
-                throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
-            }
-            if(jwtData.ua !== req.header["user-agent"]){
-                throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
-            }
-            const administrator = await this.administratorService.getById(jwtData.administratorId);
-            if(!administrator){
-                throw new HttpException('Account not found', HttpStatus.UNAUTHORIZED);
-            }
-                
-                const trenutniTimestamp = new Date().getTime()/ 1000;
-
-                if(trenutniTimestamp >= jwtData.ext){
-                    throw new HttpException('The token has expired', HttpStatus.UNAUTHORIZED);
-                }
+    async use(req: Request, res: Response, next: NextFunction) {
+    
+        if (!req.headers.authorization) {
+          throw new HttpException('Token not found', HttpStatus.UNAUTHORIZED)
+        }
         
-            next();
+        const tokenParts = req.headers.authorization.split(' ')
+    
+        if (tokenParts.length !== 2) {
+          throw new HttpException('Token does not exist!', HttpStatus.UNAUTHORIZED)
+        }
+    
+        const token = tokenParts[1]
+        let jwtData: JwtDataAdmnistratorDto
+    
+        try {
+    
+          jwtData = jwt.verify(token, jwtSecret)
+    
+        } catch (e) {
+    
+          throw new HttpException('Token is not valid!', HttpStatus.UNAUTHORIZED);
+          
+        }
+    
+        if (!jwtData) {
+          throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED)
+        }
+    
+        if (jwtData.ip !== req.ip) {
+          throw new HttpException('Bad ip address', HttpStatus.UNAUTHORIZED)
+        }
+    
+        if (jwtData.ua !== req.headers['user-agent']) {
+          throw new HttpException('Bad user agent', HttpStatus.UNAUTHORIZED)
+        }
+    
+        const administrator = await this.administratorService.getById(jwtData.administratorId)
+    
+        if (!administrator){
+          throw new HttpException('Account not found', HttpStatus.UNAUTHORIZED)
+        }
         
-
-    }
+        const currentTime = new Date().getTime() / 1000
+    
+        if (currentTime >= jwtData.ext) {
+          throw new HttpException('Token has expired', HttpStatus.UNAUTHORIZED)
+        }
+    
+        next()
+      }
 }
